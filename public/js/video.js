ef06b793d03b3aaf6b02032a7457c8e4f8fed800
Leighton Tidwell <leighton.tidwell57@gmail.com>
2021-05-10 21:13:45 -0400

    [Fix] Issue 3

--- public/js/video.js
+++ public/js/video.js
@@ -63,7 +63,7 @@ const populateRelated = (data) => {
       relatedVideoLink.forEach(link => {
         link.addEventListener("click", function (e) {
           const videoId = e.target.id.split("-")[1] || "";
-          const videoTitle = document.querySelector(`#title-${videoId}`).value;
+          const videoTitle = document.querySelector(`#title-${videoId}`).innerHTML;
           downloadVideo(videoId, videoTitle);
         });
       });
